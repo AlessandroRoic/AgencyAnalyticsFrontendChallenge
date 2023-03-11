aca615955c63a36b3fe45a283bd198cb8b29712c
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-03-11 15:44:14 -0500

    feat(API): adds gallery slice & app redux store

---
+++ src/features/gallery/gallerySlice.ts
@@ -0,0 +1,79 @@
+import {ActionReducerMapBuilder, createAsyncThunk, createSlice,} from "@reduxjs/toolkit";
+import {GalleryImage} from "./models/GalleryImage.interface";
+import GalleryAPI from "./GalleryAPI";
+import {NoInfer} from "react-redux";
+import {RootState} from "../../app/store";
+
+export enum GalleryImagesStatus {
+  LOADING,
+  IDLE,
+  FAILED,
+  FULLFILLED,
+}
+
+export interface GalleryState {
+  imagesStatus: GalleryImagesStatus;
+  images: GalleryImage[];
+  selectedImage: GalleryImage | null;
+  selectedTab: GalleryTab;
+}
+
+export enum GalleryTab {
+  RECENT,
+  FAVOURITE,
+}
+
+const initialState: GalleryState = {
+  imagesStatus: GalleryImagesStatus.IDLE,
+  images: [],
+  selectedImage: null,
+  selectedTab: GalleryTab.RECENT,
+};
+
+// REDUCERS
+
+const reducers = {};
+
+export const fetchImages = createAsyncThunk(
+  "gallery/fetchImages",
+  async (arg, { signal }) => {
+    return await GalleryAPI.getImages(signal);
+  }
+);
+
+const extraReducers = (
+  builder: ActionReducerMapBuilder<NoInfer<GalleryState>>
+) => {
+  builder
+    .addCase(fetchImages.pending, (state) => {
+      state.imagesStatus = GalleryImagesStatus.LOADING;
+    })
+    .addCase(fetchImages.fulfilled, (state, action) => {
+      state.imagesStatus = GalleryImagesStatus.FULLFILLED;
+      state.images = action.payload;
+    })
+    .addCase(fetchImages.rejected, (state, action) => {
+      state.imagesStatus = GalleryImagesStatus.FAILED;
+      console.log(action.error.message);
+    });
+};
+
+// SELECTORS
+
+export const getImages = (state: RootState): GalleryImage[] =>
+    state.gallery.images;
+
+export const getStatus = (state: RootState): GalleryImagesStatus =>
+    state.gallery.imagesStatus;
+
+// SETUP
+export const gallerySlice = createSlice({
+  name: "gallery",
+  initialState,
+  reducers,
+  extraReducers,
+});
+
+export const {} = gallerySlice.actions;
+
+export default gallerySlice.reducer;
