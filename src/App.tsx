f47e31cf511acb4a47c26df84cb4e3685ee59aee
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-03-11 15:42:59 -0500

    feat: inits react project with dependencies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Gallery from "./features/gallery/Gallery";
+
+function App() {
+  return (
+    <div className="App">
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
