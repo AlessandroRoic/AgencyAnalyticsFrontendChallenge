119130c925921b1de079fd5fccb47ff8fa13bebb
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-03-15 14:35:06 -0400

    feat(test): adds test

--- src/features/gallery/Gallery.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Gallery from "./Gallery";
-
-test("gallery loads correctly", () => {
-  render(<Gallery />);
-  const headingLabel = screen.getByText("Photos");
-  expect(headingLabel).toBeVisible();
-});
--- src/features/gallery/GalleryGrid/GalleryGrid.test.tsx
+++
@@ -1,5 +0,0 @@
-import { render } from "@testing-library/react";
-
-test("gallery grid loads correctly", () => {
-  render(<div></div>);
-});
